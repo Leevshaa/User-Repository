24269e394a0453aeb88f793b15383be67fb8bd76
Vitalii <vitaliti.yurchenko@gmail.com>
2024-12-01 23:55:02 +0200

    Add find-method

--- src/app/UserRepository.java
+++ src/app/UserRepository.java
@@ -6,14 +6,30 @@ import java.util.Optional;
 
 public class UserRepository {
 
-    List<User> users = Arrays.asList(
+    private List<User> users = Arrays.asList(
             new User(1000, "Valentin", "valentin@gmail.com"),
             new User(1001, "Olya", "olya@gmail.com"),
             new User(1002, "Katerina", "katerina@gmail.com"),
             new User(1003, "Vadim", "vadim@gmail.com")
     );
 
-    private static int findUserById(int id) {
+    public UserRepository(List<User> users) {
+        this.users = users;
+    }
 
+    private Optional<User> findUserById(int id) {
+        return users.stream().filter(user -> user.getId() == id).findFirst();
+    }
+
+    private Optional<User> findUserByEmail(String email) {
+        return users.stream().filter(user -> user.getEmail().equals(email)).findFirst();
+    }
+
+    private Optional<List<User>> findAllUsers() {
+        if (users.isEmpty()) {
+            return Optional.empty();
+        } else {
+            return Optional.of(users);
+        }
     }
 }
